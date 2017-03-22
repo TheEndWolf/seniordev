eb37dd5d542cf298cd24e11462301d7abedded8f
Niksa8 <niksa.simic8@gmail.com>
2017-03-22 01:13:12 +0100

    Update enterData.php

--- enterData.php
+++ enterData.php
@@ -3,35 +3,41 @@
 require_once ("sqlDatabase.php");
 
 class enterData{
-	
+
  private $db;
 
 	/*
 	*	construct function	
-	*	Is called as soon as a instance of this class is made.
 	*	creates connection to a database.
 	*/
 	public function __construct()
 	{
-		 $this->db = new sqlDatabase("localhost","root","","pascal_final");
+		 $this->db = new sqlDatabase("localhost","root","","pascal_database");
 	}
 	
 	/*
-	*	method for entering grades
-	*	@params: program name, program objective, course name, term, course number, CAI, overThis, % expected to achieve objective 	
+	*	method for entering report data
+	*		
 	*/
-	public function enterData($program_name, $prog_objective, $course_name, $term, $course_num, $CAI,  $overThis, $expected_per_achieved){	
+	public function enterReportData($program_name, $prog_objective, $course_name, $term, $course_num, $CAI,  $overThis, $expected_per_achieved){
+		$programID = $this->db->selectStmt_ID("Select program_id from program where program_name = '".$program_name."'");
 		$hasProgram = $this->db->selectStmt_ID("Select count(program_id) from program where program_name = '".$program_name."'");	
-		if($hasProgram = 1){
+		if($hasProgram == 1){
 			$q1 = $this->db->queryStmt("UPDATE program set program_objective = '". $prog_objective."' where program_id = ". $programID);	
 		}else{
-			$q3 = $this->db->queryStmt("INSERT into program(program_name, program_objective)values('$program_name', '$prog_objective')");		
+			$q3 = $this->db->queryStmt("INSERT into program(program_name, program_objective)values('$program_name', '$prog_objective')");	
+			$programID = $this->db->selectStmt_ID("Select program_id from program where program_name = '".$program_name."'");	
 		}		
-		$programID = $this->db->selectStmt_ID("Select program_id from program where program_name = '".$program_name."'");	
-		$q2 = $this->db->queryStmt("INSERT into course(course_name, term, course_number, program_id)values('$course_name', '$term', $course_num, $programID)");
+		$hasCourse = $this->db->selectStmt_ID("Select count(course_id) from course where course_name = '".$course_name."' and course_number = ". $course_num . " and term = ".$term);
+		if($hasCourse == 1){
+			$courseID = $this->db->selectStmt_ID("Select course_id from course where course_name = '".$course_name."'");
+			$q11 = $this->db->queryStmt("UPDATE course set course_name = '". $course_name."', course_number = ". $course_num .", term = ". $term ." where course_id = ". $courseID);
+		}else{
+		$q2 = $this->db->queryStmt("INSERT into course(course_name, term, course_number, program_id)values('$course_name', $term, $course_num, $programID)");
 		$courseID = $this->db->selectStmt_ID("Select course_id from course where course_name = '".$course_name."'");
-		$q4 = $this->db->queryStmt("INSERT into assessment(course_assessment_item, over_this, expected_percent_achieved, course_id)values('$CAI', $overThis, $expected_per_achieved, $courseID)");		
-				if($q1 && $q2 && $q3 && $q4){
+		}		
+				$q4 = $this->db->queryStmt("INSERT into assessment(course_assessment_item, over_this, expected_percent_achieved, course_id)values('$CAI', $overThis, $expected_per_achieved, $courseID)");				
+				if($q4){
 					echo "success: inserted data";
 				}else{
 					echo "error: did not insert data";
@@ -40,12 +46,12 @@ class enterData{
 	
 	/*
 	*	method for entering scores
-	*	@params: program name, program objective, courese name, term, course number, section, CAI, % expected to achieve objective 	
+	*		
 	*/
 	public function enterScore($first_name, $lastName, $course_name, $courseNum, $assessmentName, $score){	
 		$studentID = $this->db->selectStmt_ID("Select student_id from student where first_name = '".$first_name."' and last_name = '". $lastName."'");
 		$courseID = $this->db->selectStmt_ID("Select course_id from course where course_name = '".$course_name."' and course_number = ".$courseNum);
-		$assessmentID = $this->db->selectStmt_ID("Select assessment_id from assessment where assessmentName = '".$assessmentName."' and course_id = '".$courseID."'");
+		$assessmentID = $this->db->selectStmt_ID("Select assessment_id from assessment where course_assessment_item = '".$assessmentName."' and course_id = ".$courseID);
 		$q1 = $this->db->queryStmt("INSERT into score(score, assessment_id, student_id)values($score, $assessmentID, $studentID)");	
 				if($q1){
 					echo "success: inserted scores";
@@ -54,21 +60,22 @@ class enterData{
 				}
 	}
 	
-		/*
+	/*
 	*	method for entering grades
-	*	@params: program name, program objective, courese name, term, course number, section, CAI, % expected to achieve objective 	
+	*	
 	*/
 	public function enterGrade($first_name, $last_name, $course_name, $sectionNum, $charGrade, $grade){	
 		$studentID = $this->db->selectStmt_ID("Select student_id from student where first_name = '".$first_name."' and last_name = '". $last_name."'");
 		$courseID = $this->db->selectStmt_ID("Select course_id from course join course_section using(course_id) join section using(section_id) where course_name = '".$course_name."' and section_number = ".$sectionNum);
-		$q2 = $this->db->queryStmt("INSERT into grade(charGrade, grade)values($charGrade, $grade)");
-		$gradeID = $this->db->insert_id;
+		$q2 = $this->db->queryStmt("INSERT into grade(charGrade, grade)values('$charGrade', $grade)");
+		$gradeID = $this->db->selectStmt_ID("select grade_id from grade order by grade_id DESC LIMIT 1");
 		$q3 = $this->db->queryStmt("INSERT into grade_student(grade_id, student_id)values($gradeID, $studentID)");	
 				if($q2 && $q3){
 					echo "success: inserted grade";
 				}else{
 					echo "error: did not insert grade";
 				}
+			
 	}
 
 }
