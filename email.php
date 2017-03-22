c4ceffdaf9ef6eb749866b751c7ff46cc81f9c36
Niksa8 <niksa.simic8@gmail.com>
2017-03-22 01:12:25 +0100

    Update email.php

--- email.php
+++ email.php
@@ -6,28 +6,28 @@ class email{
 	
  private $db;
 
-  public $subject = "Report";
+  public $subject = "the subject is: Report";
   public $header = "buu";
   public $msg = "Confirming the competion of you report.";
 
 
 	/*
 	*	construct function	
-	*	Is called as soon as a instance of this class is made.
 	*	creates connection to a database.
 	*/
 	public function __construct()
 	{
-		 $this->db = new sqlDatabase("localhost","root","","pascal_final");
+		 $this->db = new sqlDatabase("localhost","root","","pascal_database");
 	}
 	
 	/**
 	*	function sends email to confirm completion of evaluation
 	*/	
-		public function sendEmail($username){
+	public function sendEmail($username){
 		$userEmail= $this->db->selectStmt_ID("select userEmail from program_user where username = '". $username ."'");
 		mail($userEmail, $this->subject, $this->msg, $this->header);
 	}
+
 
 }
 
