4dd125a572ed4f87c8f00661f9f735284245d2e1
Niksa8 <niksa.simic8@gmail.com>
2017-03-22 01:10:28 +0100

    Update report.php

--- report.php
+++ report.php
@@ -7,62 +7,54 @@ class report{
  private $db;
 	/*
 	*	construct function	
-	*	Is called as soon as a instance of this class is made.
 	*	creates connection to a database.
 	*/
 	public function __construct()
 	{
-		 $this->db = new sqlDatabase("localhost","root","","pascal_final");
+		 $this->db = new sqlDatabase("localhost","root","","pascal_database");
 	}
 
 	/*
 	*	function to generate report
-	*	Takes no parameters
 	*	Retrieves data from database
 	*/
-	public function generateReport($courseName, $secNum){
-		$sectionID = $this->db->selectStmt_ID("select section.section_id from section, course where course_name = '". $courseName ."' and sectionNum = ". $sectionNum);
-		$result = $this->db->selectStmt_Assoc("select program_name, course_name, term from program, course where course_name = '". $courseName ."' and section_id = ". $sectionID);
+	public function generateReport($courseName, $sectionNum){
+		$sectionID = $this->db->selectStmt_ID("select section.section_id from section, course where course_name = '". $courseName ."' and section_number = ". $sectionNum);
+		$result = $this->db->selectStmt_Assoc("select program.program_name, course.course_name, course.term from program, course, section where course_name = '". $courseName ."' and section_id = ". $sectionID);
 	}
 
 	/*
-	*	function to generate report
-	*	Takes no parameters
+	*	function to get report
 	*	Retrieves data from database
 	*/
 	public function getReport(){
-		$result = $this->db->selectStmt_Assoc("select program.program_name, program.program_objective, course.course_name, course.term, course.course_number, section.section_number, assessment.assessmentName, assessment.expected_Percent_achieved
-from program, course, course_section, section, assessment");
+		$result = $this->db->selectStmt_Assoc("select program_name, program_objective, course_name, term, course_number, section_number, assessmentName, expected_Percent_achieved from program join course using (program_id) join course_section using (course_id) join section using(section_id) join assessment using(course_id)");
+
 	}
 
 	/*
 	*	function to generate course report
-	*	@param - course name
-	*	Retrieves data from database, course name, assessment name, assessment notes, expected % of students that achieved the objective, expected % of students to achieve the objective
 	*/
 	public function getCourseData($courseName, $sectionNum){
-		$result = $this->db->selectStmt_Assoc("select program_name, course_name, term, avgGrade_expected, from course join program using (program_id) join course_section using(course_id) join section using(section_id) where course_name = '".$courseName."' and section_number = ".$sectionNum);
+		$result = $this->db->selectStmt_Assoc("select program_name, course_name, term from course join program using (program_id) join course_section using(course_id) join section using(section_id) where course_name = '".$courseName."' and section_number = ".$sectionNum);
 	}
 
 	/*
-	*	returs the percentage of students that passed this assessment
-	*	returns the average score
-	*	@param - course name, assessment name
+	*	returns the percentage of students that passed this assessment
 	*/
 	public function taskStreamReport($courseName, $sectionNum){
 		$avg =0;
 		$passed=0;
 		$iNum = 0;
 		
-		$courseID = $this->db->selectStmt_ID("select course_id from course, section where course_name = '" . $courseName . " and section_number = '". $sectionNum ."'");
+		$courseID = $this->db->selectStmt_ID("select course.course_id from course, section where course_name = '" . $courseName . "' and section_number = ". $sectionNum);
 		$assessments = $this->db->selectStmt_ID("select assessment_id from assessment where course_id = ". $courseID);
 		$assessmentsCOUNT = $this->db->selectStmt_ID("select count(assessment_id) from assessment where course_id = ". $courseID);
-
 				for($x = 0; $x < $assessmentsCOUNT; $x++) {
-					$assID = assessments[$iNum];
+					$assID = $assessments[$iNum];
 					$assname = $this->db->selectStmt_ID("select assessmentName from assessment where assessment_id = ". $assID);
-					$overThis = $this->db->selectStmt_ID("select over_this from assessment join course using(course_id) where assessment_id = ". $assID)
-					$scores = $this->db->selectStmt_Arr("select score from student join score using(student_id) join assessment using(assessment_id) join course using(course_id) join course_student using(course_id) join course_section using(course_id) join section using(sectio_id) where assessment_id = ".$assID."'");	
+					$overThis = $this->db->selectStmt_ID("select over_this from assessment join course using(course_id) where assessment_id = ". $assID);
+					$scores = $this->db->selectStmt_Arr("select score from student join score using(student_id) join assessment using(assessment_id) join course using(course_id) join course_student using(course_id) join course_section using(course_id) join section using(section_id) where assessment_id = ".$assID);	
 					$iNum++;
 					$count = count($scores);
 					if($count == 0){
@@ -84,7 +76,6 @@ from program, course, course_section, section, assessment");
 	
 	/*
 	*	imports data from csv file to mysql
-	*	@param - file name and table name in db
 	*	must have these fields in csv: report_id,program_name,program_objective,course_name,term,course_number,section,assessmentName,expected_percent_achieved,percent_students_achieved_obj
 	*/
 	public function uploadReport($filename){
