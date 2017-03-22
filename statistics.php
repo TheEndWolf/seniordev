10f407c324ff6d743a016a109271cf185afd46bd
Niksa8 <niksa.simic8@gmail.com>
2017-03-22 01:15:28 +0100

    Update statistics.php

--- statistics.php
+++ statistics.php
@@ -8,59 +8,51 @@ class statistics{
 
 	/*
 	*	construct function	
-	*	Is called as soon as a instance of this class is made.
 	*	creates connection to a database.
 	*/
 	public function __construct()
 	{
-		 $this->db = new sqlDatabase("localhost","root","","pascal_final");
+		 $this->db = new sqlDatabase("localhost","root","","pascal_database");
 	}
 	
 	/*
-	*	returs the percentage of students that passed this assessment
-	*	returns the average score
-	*	@param - course name, assessment name
+	*	returns the the average grade and percentage of students that passed this assessment
 	*/
 	public function customizedStatistics_changeOverThis($courseName, $sectionNum, $changeExpectedGrade){
 		$avg =0;
-		$passed=0;
 		$iNum = 0;
-		
-		$courseID = $this->db->selectStmt_ID("select course_id from course, section where course_name = '" . $courseName . " and section_number = '". $sectionNum ."'");
-		$assessments = $this->db->selectStmt_ID("select assessment_id from assessment where course_id = ". $courseID);
+		$courseID = $this->db->selectStmt_ID("select course.course_id from course, section where course_name = '" . $courseName . "' and section_number = ". $sectionNum);
+		$assessments = $this->db->selectStmt_Arr("select assessment_id from assessment where course_id = ". $courseID);
 		$assessmentsCOUNT = $this->db->selectStmt_ID("select count(assessment_id) from assessment where course_id = ". $courseID);
-
 				for($x = 0; $x < $assessmentsCOUNT; $x++) {
-					$assID = assessments[$iNum];
+					$passed=0;
+					$assID = $assessments[$iNum];
 					$assname = $this->db->selectStmt_ID("select assessmentName from assessment where assessment_id = ". $assID);
-					$overThis = $this->db->selectStmt_ID("select over_this from assessment join course using(course_id) where assessment_id = ". $assID)
-					$scores = $this->db->selectStmt_Arr("select score from student join score using(student_id) join assessment using(assessment_id) join course using(course_id) join course_student using(course_id) join course_section using(course_id) join section using(sectio_id) where assessment_id = ".$assID."'");	
+					$overThis = $this->db->selectStmt_ID("select over_this from assessment join course using(course_id) where assessment_id = ". $assID);
+					$scores = $this->db->selectStmt_Arr("select score from student join score using(student_id) join assessment using(assessment_id) join course using(course_id) join course_student using(course_id) join course_section using(course_id) join section using(section_id) where assessment_id = ". $assID);	
 					$iNum++;
 					$count = count($scores);
-					if($count == 0){
-						echo "No results!";
-						return;
-					}
+					if($count !== 0){					
 						for ($i = 0; $i < $count; $i++) {
 							$avg = $avg + $scores[$i];
 							$sco = $scores[$i];
-							if($sco > $changeExpectedGrade){//$overThis
+							if($sco > $changeExpectedGrade){
 								$passed++;
 							}
 						}
 					$result = ($passed / $count) * 100;
 					$str = $result . "% of student recieved higher than the expected average grade of " . $changeExpectedGrade . "% on the " . $assname . "<br>";
 					echo $str;
+					}else echo "no results <br>";
 				}
 	}
 
 	/*
-	*	function to generate report
-	*	Takes no parameters
-	*	Retrieves data from database
+	*	function to generate statistics
+	*	Retrieves data from database, compares expected Percent Achieved with percent achieved
 	*/
 	public function showStatistics(){
-		$result = $this->db->selectStmt_Report("Select expectedPercentAchieved, percent_achieved_Obj from course");
+		$result = $this->db->selectStmt_Report("Select expected_percent_achieved, percent_students_achieved from assessment");
 	}
 
 
