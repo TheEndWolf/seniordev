5657c5fc64f2307461c95b55b322abef47f0196b
Mitch Steenburgh <mfsteenburgh@gmail.com>
2017-05-01 16:15:26 -0400

    Changes to gettingData.php, adding more functions to add drop downs

--- inc/php/gettingData.php
+++ inc/php/gettingData.php
@@ -20,6 +20,43 @@ class gettingData{
 	*/	
 	public function getClasses(){
 		$getClasses= $this->db->selectStmt_Arr("SELECT course_name FROM course");
+		$getClassId = $this->db->selectStmt_Arr("SELECT course_id FROM course");
+		$arrCount= count($getClasses);
+		$option = '<p>Class: <select name="courseNameee" class="form-control">';
+		//$option .= '<option value = "''">'Classes'</option>';
+			for($x = 0; $x < $arrCount; $x++) {
+				$option .= '<option value = "'.$getClassId[$x].'">'.$getClasses[$x].'</option>';
+			}
+			$option.= '</select></p>';
+		echo $option;
+	}
+	
+	/**
+	*	function places term numbers into select component
+	*/	
+	public function getTerms($courseId){
+		$getSections= $this->db->selectStmt_Arr("SELECT section_id FROM course_section WHERE course_id = '" .$courseId. "'");
+		$termInfo = 'WHERE section_id = "'.$getSections[0].'"';
+		for($x = 1; $x < count($getSections); $x++)
+		{
+			$termInfo .= ' OR section_id = "'.$getSections[$x].'"';
+		}
+		$getTerms= $this->db->selectStmt_Arr("SELECT term FROM section ".$termInfo);
+		$arrCount= count($getTerms);
+		$option = '<p>Terms: <select name="terms" class="form-control">';
+		//$option .= '<option value = "''">'Classes'</option>';
+			for($x = 0; $x < $arrCount; $x++) {
+				$option .= '<option value = "'.$getTerms[$x].'">'.$getTerms[$x].'</option>';
+			}
+			$option.= '</select></p>';
+		echo $option;
+	}
+	
+	/**
+	*	function places section numbers into select component
+	*/	
+	public function getSections($courseId){
+		/*$getClasses= $this->db->selectStmt_Arr("SELECT course_name FROM course");
 		$arrCount= count($getClasses);
 		$option = '<p>Class: <select name="courseNameee" class="form-control">';
 		//$option .= '<option value = "''">'Classes'</option>';
@@ -36,10 +73,8 @@ class gettingData{
 				$option1 .= '<option value = "'.$getSection[$x].'">'.$getSection[$x].'</option>';
 			}
 			$option1.= '</select></p>';
-		echo $option1;
+		echo $option1;*/
 	}
-
-
 
 }
 
