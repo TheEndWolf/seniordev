e9389499583c911e559ccdf13707ac8540705090
Mitchell Steenburgh <mfsteenburgh@gmail.com>
2017-05-03 19:14:25 -0400

    Removed RIT links on login

--- index.php
+++ index.php
@@ -8,12 +8,8 @@ if (!array_key_exists('loggedIn', $_COOKIE)) {
     session_destroy();
 } else {
     $expire = time() + 60 * 10;//10 minutes from now
-    //Deployment
     $path = "/";
     $domain = "team-pascal.ist.rit.edu";
-    //Testing
-//        $path = "/~speedyman11/srdev2/";
-//        $domain = "172.110.20.237";
     $secure = false;
     $value = date("F j, Y g:i a");
     $value = mt_rand() . mt_rand() . mt_rand();
@@ -64,7 +60,7 @@ if (array_key_exists('role_id', $_SESSION)) {
      <form method=\"post\" action='./inc/php/login.php'>
             <p>
               <label><b>Username</b></label>
-              <input type=\"text\" name=\"username\" placeholder=\"RIT Username (Do not include @rit.edu)\" class=\"form-control log-in\" />
+              <input type=\"text\" name=\"username\" placeholder=\"Username\" class=\"form-control log-in\" />
             </p>
             <p>
               <label><b>Password</b></label>
@@ -72,15 +68,11 @@ if (array_key_exists('role_id', $_SESSION)) {
             </p>
 
 
-            <input type=\"submit\" name=\"login\" value=\"Login\">
-
-            <a href=\"https://start.rit.edu/ForgotUsername?source=shib\">Forgot Username?</a> |
-            <a href=\"https://start.rit.edu/ForgotPassword?source=shib\">Forgot Password?</a>
+            <input class=\"btn btn-success\" type=\"submit\" name=\"login\" value=\"Login\">
 
              </form>
       </div>";
     }
-    //<button class=\"btn btn-success\" name=\"login\">Log in</button>
     ?>
     <div id="clear"></div>
 </div>
