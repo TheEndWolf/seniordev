9f129533ff9e8bb389c8cc707293ea8dda3610a4
dgo3015 <dgo3015@g.rit.edu>
2017-05-03 18:56:00 -0400

    cleanup

--- inc/js/ajaxFunc.js
+++ inc/js/ajaxFunc.js
@@ -1,27 +1,3 @@
-
-/*
-$(document).ready(function(){
-	$("form").submit(function(){
-	/*	var username = $('#class_taskstream').val();
-		var mail = $('#section_taskstream').val();
-
-		var dataa = 'username' + username + ' mail' + mail;
-		console.log(dataa);
-	 
-		$.ajax({
-			type: "POST",
-			url: "saveToDB.php",
-			data: ({name: 124}),
-			success: function(data){
-				console.log(data);
-				alert("submited");
-			}
-		});
-	});
-});
-*/
-
-
 $(document).ready(function(){
     $("#taskStream").on("click",function(){
         //alert("Submitted");
